372c84a6e3b2c635b0cbfba39ac92a03af3bdfe2
utahisnotastate <utahisnotastate@gmail.com>
2021-12-28 01:49:10 -0500

    added boilerplate

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import DidIDoIt from "./src/dididoit";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Development</Text>
+
       <StatusBar style="auto" />
+      <DidIDoIt />
     </View>
   );
 }
